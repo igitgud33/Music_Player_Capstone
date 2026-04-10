49e68445d09fb3660c52ed5ee36cc835dbddf5c9
Jerry <Jerrygidayawan15@gmail.com>
2026-04-10 21:53:19 +0800

    Initial setup of MediaPlayer class

---
+++ Project/core/MediaPlayer.java
@@ -0,0 +1,8 @@
+package Music_Player_Capstone.Project.core;
+import java.util.Scanner;
+public class MediaPlayer {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello World!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Music_Player_Capstone
 Final project
+Hello Everyone. You can start creating the basic classes lang sa.
