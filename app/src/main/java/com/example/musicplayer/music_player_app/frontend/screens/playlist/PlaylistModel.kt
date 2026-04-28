d44fa6c60859314542e2641b3d97e4e2eee0a520
Jerry <Jerrygidayawan15@gmail.com>
2026-04-28 09:37:33 +0800

    Testing file

--- app/src/main/java/com/example/musicplayer/music_player_app/frontend/screens/playlist/PlaylistModel.kt
+++ app/src/main/java/com/example/musicplayer/music_player_app/frontend/screens/playlist/PlaylistModel.kt
@@ -3,9 +3,9 @@ package com.example.musicplayer.music_player_app.frontend.screens.playlist
 class PlaylistModel : PlaylistContract.Model {
     override fun fetchSongs(callback: (List<Song>?, Throwable?) -> Unit) {
         val mockSongs = listOf(
-            Song("Bohemian Rhapsody", "Queen", ""),
-            Song("Hotel California", "Eagles", ""),
-            Song("Stairway to Heaven", "Led Zeppelin", "")
+            Song("test", "test", ""),
+            Song("test", "test", ""),
+            Song("test", "test", "")
         )
 
         callback(mockSongs, null)
